49e18571257b96690131f42aa50df9b49a70232f
Sam Course <notcourseps@gmail.com>
2022-06-11 12:13:12 +0200

    Added _currentShape to Game.cs
    Games now have their own Shape class called _currentShape.

--- Tetris/TetrisEngine/Game.cs
+++ Tetris/TetrisEngine/Game.cs
@@ -7,6 +7,7 @@ namespace TetrisEngine {
         private int _rows;
         private int _columns;
         private List<Matrix> _queue { get; }
+        private Shape _currentShape;
         
         public TetrisGame(int rows, int columns) {
             _rows = rows;
@@ -23,6 +24,22 @@ namespace TetrisEngine {
                 QueueNewShape();
             }
         }
+
+        /// <summary>
+        /// Moves the next tetromino from the queue to the board and adds a new random tetromino to the queue.
+        /// </summary>
+        private void SpawnNextTetromino() {
+            // Get and removes the next tetromino from the queue
+            Matrix matrix = _queue[0];
+            _queue.RemoveAt(0);
+            
+            // Add a new tetromino to the queue
+            QueueNewShape();
+
+            // Create a new shape with the randomly picked tetromino and the default starting position
+            _currentShape = new Shape(matrix, _columns / 2 - 1, 0);
+        }
+
         /// <summary>
         /// Adds a new random tetromino to the queue.
         /// </summary>

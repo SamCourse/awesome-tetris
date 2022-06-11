92a9b430d9e0e815734a5aa41b68bc7af35d9484
Sam Course <notcourseps@gmail.com>
2022-06-11 12:09:03 +0200

    Create Game.cs
    Game.cs has all game logic belonging to the Tetris game instance.
    InitializeGame(), as the name suggests, initializes the Game object. This prepares the instance for being ready-to-play.
    QueueNewShape() adds a new random shape to the queue.

---
+++ Tetris/TetrisEngine/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TetrisEngine {
+    
+    public class TetrisGame {
+        private int _rows;
+        private int _columns;
+        private List<Matrix> _queue { get; }
+        
+        public TetrisGame(int rows, int columns) {
+            _rows = rows;
+            _columns = columns;
+            _queue = new List<Matrix>();
+        }
+
+        /// <summary>
+        /// Initializes the game. TODO: More info
+        /// </summary>
+        public void InitializeGame() {
+            // Queue 3 shapes
+            for (int i = 0; i < 3; i++) {
+                QueueNewShape();
+            }
+        }
+        /// <summary>
+        /// Adds a new random tetromino to the queue.
+        /// </summary>
+        private void QueueNewShape() {
+            _queue.Add(Shapes.RandomShape());
+        }
+    }
+
+}

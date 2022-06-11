d3cff9320f9d537339ff06829572e9c4a0a68d0c
Sam Course <notcourseps@gmail.com>
2022-06-11 12:21:42 +0200

    Added TetrisGame instance to the MainWindow

--- Tetris/TetrisClient/MainWindow.xaml.cs
+++ Tetris/TetrisClient/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TetrisEngine;
+using Shape = TetrisEngine.Shape;
 
 namespace TetrisClient
 {
@@ -20,6 +15,10 @@ namespace TetrisClient
     /// </summary>
     public partial class MainWindow : Window
     {
+    public partial class MainWindow : Window {
+
+        private TetrisGame _game;
+
         public MainWindow()
         {
             InitializeComponent();

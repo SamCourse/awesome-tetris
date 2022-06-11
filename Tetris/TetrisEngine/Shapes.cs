32fedd203446d62dc36dade9069979ee12eaabf2
Sam Course <notcourseps@gmail.com>
2022-06-11 12:03:43 +0200

    Added Shapes.cs with pre-defined Shapes as matrices
    The static class has a static readonly list of pre-written Matrices according to all the tetrominoes in Tetris.
    Method RandomShape() can be used to choose a random shape from this list.

---
+++ Tetris/TetrisEngine/Shapes.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TetrisEngine {
+
+    public static class Shapes {
+        /// Shape reference (https://tetris.fandom.com/wiki/Tetromino)
+        /// 
+        /// <summary>
+        /// A list of Shapes used in Tetris. They are represented as Matrixes.
+        /// Shapes have a corresponding number. This is used to be able to identify different shapeson a board.
+        /// I: 1
+        /// J: 2
+        /// L: 3
+        /// O: 4
+        /// S: 5
+        /// T: 6
+        /// Z: 7
+        /// </summary>
+        private static readonly List<Matrix> shapes = new() {
+            // I  ▀▀▀▀ 
+            // Define the matrix coordinates
+            new Matrix(new int[,] {
+                { 1, 1, 1, 1 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            }),
+
+
+            // J  █▄▄
+            new Matrix(new int[,] {
+                { 2, 0, 0 },
+                { 2, 2, 2 },
+                { 0, 0, 0 }
+            }),
+
+            // L  ▄▄█
+            new Matrix(new int[,] {
+                { 0, 0, 3 },
+                { 3, 3, 3 },
+                { 0, 0, 0 }
+            }),
+
+            // O  ██
+            new Matrix(new int[,] {
+                { 4, 4 },
+                { 4, 4 }
+            }),
+
+            // S  ▄█▀
+            new Matrix(new int[,] {
+                { 0, 5, 5 },
+                { 5, 5, 0 },
+                { 0, 0, 0 },
+            }),
+
+            // T  ▄█▄
+            new Matrix(new int[,] {
+                { 0, 6, 0 },
+                { 6, 6, 6 },
+                { 0, 0, 0 },
+            }),
+
+            // Z  ▀█▄
+            new Matrix(new int[,] {
+                { 7, 7, 0 },
+                { 0, 7, 7 },
+                { 0, 0, 0 },
+            })
+        };
+
+        /// <summary>
+        ///  Chooses a random shape from the list of pre-defined Tetris shapes
+        /// </summary>
+        /// <returns> A random shape in the form of a Matrix </returns>
+        public static Matrix RandomShape() {
+            return shapes[new Random().Next(shapes.Count)];
+        }
+    }
+}
